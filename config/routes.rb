750d0d3db3a1b02feca3ca480ff47ae09df9516d
geoffrey <geoffrey.tisserand@gmail.com>
2019-02-04 09:01:07 -0800

    Login page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   
 	root to: "offers#index"
 
+	get  "login",                    to: "users#login"
+	post "login",                    to: "users#post_login"
+
   get  "companies/:id",            to: "companies#show"
   get  "companies/:id/offers/new", to: "offers#new"
   post "companies/:id/offers",     to: "offers#create"
