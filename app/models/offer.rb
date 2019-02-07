b3c477db472e66af8e0b827aac6543b7cce2ded7
geoffrey <geoffrey.tisserand@gmail.com>
2019-02-06 18:01:52 -0800

    Offer status

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,19 +5,37 @@ class Offer < ApplicationRecord
   has_many :votes
   has_many :comments
 
+  before_create :set_default_values
+
   validates :status, inclusion: { in: %w(pending accepted rejected) }
 
-  def job_offer_type_class
-    return "success" if status == :accepted
-  	return "danger" if status == :rejected
-  	"info"
+  def status_class
+    return "success" if status == "accepted"
+    return "danger" if status == "rejected"
+    "info"
   end
 
   def views
-  	rand(800)
+    rand(10000)
   end
 
   def tc
-  	base_salary.to_i + signon_bonus.to_i
+    base_salary.to_i + signon_bonus.to_i
+  end
+
+  def accept!
+    self.status = "accepted"
+    self.save!
+  end
+  
+  def reject!
+    self.status = "rejected"
+    self.save!
+  end
+
+  private 
+
+  def set_default_values
+    self.status ||= "pending"
   end
 end
