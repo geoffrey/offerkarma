ec754009cafbe71d2d3e7130e784f3b3a281e031
geoffrey <geoffrey.tisserand@gmail.com>
2019-02-09 12:25:49 -0800

    Admin basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 	include SessionsHelper
 
+  def authenticate_admin_user!
+    authenticate_or_request_with_http_basic("admin") do |username, password|
+      ActiveSupport::SecurityUtils.secure_compare(username, ENV.fetch['ADMIN_USERNAME']) &
+        ActiveSupport::SecurityUtils.secure_compare(password, ENV.fetch['ADMIN_PASSWORD'])
+    end
+  end
+
 	def redirect_if_logged_in
 		redirect_to offers_path if logged_in?
 	end
