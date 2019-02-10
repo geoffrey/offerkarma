ab5b45984be62c45943cf9edfae304ca32fb3fe4
Benjamin Guillet <benjamin.guillet@gmail.com>
2019-02-09 22:26:54 -0800

    Config actionmailer for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'geoffrey@reffo.us' }
+  config.action_mailer.smtp_settings = {
+    user_name: ENV.fetch('SENDGRID_USERNAME'),
+    password: ENV.fetch('SENDGRID_PASSWORD'),
+    domain: 'reffo.us',
+    address:'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
