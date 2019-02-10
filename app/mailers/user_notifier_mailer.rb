4fced336b600b2663d620c9f29790c114ff4ad59
Benjamin Guillet <benjamin.guillet@gmail.com>
2019-02-09 22:20:40 -0800

    First mailer and shit

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,2 +1,9 @@
 class UserNotifierMailer < ApplicationMailer
+  default from: 'geoffrey@reffo.us'
+
+ # send a signup email to the user
+ def send_signup_email(user)
+   @user = user
+   mail(to: @user.email, subject: 'Thanks for signing up for our amazing app')
+ end
 end
