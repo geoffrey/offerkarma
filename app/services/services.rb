0792e7a7d2a4414b3511e7b112f491252f44a0c0
Benjamin Guillet <benjamin.guillet@gmail.com>
2019-02-10 01:24:39 -0800

    Fix prod loading

--- app/services/services.rb
+++ app/services/services.rb
@@ -2,7 +2,7 @@
 
 module Services
   def user_registration
-    @user_registration ||= Services::Users::Registration.new
+    @user_registration ||= Users::Registration.new
   end
   module_function :user_registration
 end
