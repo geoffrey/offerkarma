693c5d8e2290dda7a32e5ed5ca4994441113af5c
Benjamin Guillet <benjamin.guillet@gmail.com>
2019-02-09 23:58:00 -0800

    Actionmailer settings for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,10 +32,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -64,6 +60,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.default_options = { from: 'geoffrey@reffo.us' }
   config.action_mailer.smtp_settings = {
     user_name: ENV.fetch('SENDGRID_USERNAME'),
