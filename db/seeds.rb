057135ba9d0856942ae2262e956ae0f1e22ae21d
geoffrey <geoffrey.tisserand@gmail.com>
2019-02-09 23:35:42 -0800

    Change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 user = User.create!(email: "admin@reffo.us", password: "password", password_confirmation: "password")
+user2 = User.create!(email: "hello@reffo.us", password: "password", password_confirmation: "password")
 
 eaze = Company.create!(name: "eaze", url: "eaze.com")
 sharespost = Company.create!(name: "sharespost", url: "sharespost.com")
@@ -9,6 +10,6 @@ Company.create!(name: "remind", url: "remind.com")
 Company.create!(name: "coinbase", url: "coinbase.com")
 Company.create!(name: "stripe", url: "stripe.com")
 
-Offer.create!(user: user, company: eaze, base_salary: 185000, position: "Senior Software Engineer", status: "pending")
-Offer.create!(user: user, company: sharespost, base_salary: 180000, position: "Senior Software Engineer", status: "pending")
 Offer.create!(user: user, company: instacart, base_salary: 180000, position: "Senior Software Engineer", status: "pending")
+Offer.create!(user: user2, company: sharespost, base_salary: 180000, position: "Senior Software Engineer", status: "pending")
+Offer.create!(user: user2, company: eaze, base_salary: 185000, position: "Senior Software Engineer", status: "pending")
