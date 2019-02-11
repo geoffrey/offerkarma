a9ffb2e746fe5eb06e60be958e32c35f302865a1
geoffrey <geoffrey.tisserand@gmail.com>
2019-02-10 19:13:13 -0800

    Autocomplete

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -122,9 +122,10 @@ ActiveAdmin.setup do |config|
   #
   # You can completely disable comments:
   config.comments = false
+
   #
   # You can change the name under which comments are registered:
-  # config.comments_registration_name = 'AdminComment'
+  config.comments_registration_name = 'AdminComment'
   #
   # You can change the order for the comments and you can change the column
   # to be used for ordering:
