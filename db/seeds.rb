1498f0a6927a0912b22cec4b5f486241cdf53214
geoffrey <geoffrey.tisserand@gmail.com>
2019-02-11 18:25:09 -0800

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@ Company.create!(name: "remind", domain: "remind.com")
 Company.create!(name: "coinbase", domain: "coinbase.com")
 Company.create!(name: "stripe", domain: "stripe.com")
 
-Offer.create!(user: user, company: instacart, base_salary: 180000, stock_type: :rsu, stock_count: 20000, stock_fair_market_value: 30.00, position: "Senior Software Engineer", status: "pending")
+Offer.create!(user: user, company: instacart, base_salary: 180000, stock_type: :rsus, stock_count: 20000, stock_fair_market_value: 30.00, position: "Senior Software Engineer", status: "pending")
 Offer.create!(user: user2, company: sharespost, base_salary: 180000, stock_type: :option, stock_count: 50000, stock_fair_market_value: 2.00, stock_strike_price: 1.3, position: "Senior Software Engineer", status: "pending")
 Offer.create!(user: user2, company: eaze, base_salary: 185000, position: "Senior Software Engineer", status: "pending")
